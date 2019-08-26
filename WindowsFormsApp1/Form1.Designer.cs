86f46438e9e8ee7ce3b71d3e0c56fd8d15f7eb96
JamesW <james.wakeham@fuelgames.io>
2019-08-27 09:43:11 +1000

    init

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 231);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1741, 680);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(450, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(53, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(450, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Old SoT File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "New SoT File Path";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1837, 939);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public struct CardData
+        {
+            public string name;
+            public int strength, health;
+        }
+
+        public List<CardData> oldCardDatas = new List<CardData>();
+        public List<CardData> newCardDatas = new List<CardData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //XmlReader reader;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oldCardDatas = ParseSoT("C:/XMLs/oldTest.xml");
+            newCardDatas = ParseSoT("C:/XMLs/newTest.xml");
+            CompareSoTs(oldCardDatas, newCardDatas);
+        }
+
+        public void CompareSoTs(List<CardData> oldSoT, List<CardData> newSoT)
+        {
+            textBox1.AppendText("sot length difference: " + (oldSoT.Count - newSoT.Count) + System.Environment.NewLine);
+            for (int i = 0; i < oldSoT.Count; i++)
+            {
+                bool healthDiff = false;
+                bool strDiff = false;
+                if (oldSoT[i].health != newSoT[i].health)
+                {
+                    healthDiff = true;
+                }
+                if (oldSoT[i].strength != newSoT[i].strength)
+                {
+                    strDiff = true;
+                }
+
+                if(healthDiff || strDiff)
+                {
+                    textBox1.AppendText(oldSoT[i].name + System.Environment.NewLine);
+                    textBox1.AppendText(oldSoT[i].strength + "/" + oldSoT[i].health + " to " + newSoT[i].strength + "/" + newSoT[i].health + System.Environment.NewLine);
+                }
+            }
+        }
+
+        public List<CardData> ParseSoT (string location)
+        {
+            List<CardData> tempCardData = new List<CardData>();
+
+            // Create an XML reader for this file.
+            using (XmlReader reader = XmlReader.Create(location))
+            {
+                CardData currentCardData = new CardData();
+                while (reader.Read())
+                {
+                    // Only detect start elements.
+                    if (reader.IsStartElement())
+                    {
+                        // Get element name and switch on it.
+                        switch (reader.Name)
+                        {
+                            case "Name":
+                                // Next read will contain text.
+                                if (reader.Read())
+                                {
+                                    if (reader.Value.Trim() != currentCardData.name)
+                                    {
+                                        currentCardData = new CardData();
+                                        currentCardData.name = reader.Value.Trim();
+
+                                        tempCardData.Add(currentCardData);
+                                    }
+
+                                }
+                                break;
+
+                            case "Attack":
+                                if (reader.Read())
+                                {
+                                    int.TryParse(reader.Value.Trim(), out currentCardData.strength);
+                                }
+
+                                break;
+                            case "Health":
+                                if (reader.Read())
+                                {
+                                    int.TryParse(reader.Value.Trim(), out currentCardData.health);
+
+                                    tempCardData.Add(currentCardData);
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return tempCardData;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
